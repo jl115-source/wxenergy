9da1e51b48fca76e923eae80ba63842f3a7c7879
jl115-source <jl115@alumni.princeton.edu>
2026-09-22 16:17:46 -0700

    Expose coordinate standardization

--- src/wxenergy/__init__.py
+++ src/wxenergy/__init__.py
@@ -6,7 +6,7 @@ from .climate import anomaly, climatology
 from .energy import cdd, degree_hours, hdd
 from .ensemble import ensemble_mean, ensemble_probability, ensemble_quantile, ensemble_spread
 from .spatial import cosine_latitude_weights, weighted_mean
-from .standardize import normalize_longitude
+from .standardize import normalize_longitude, standardize
 from .temporal import daily_max, daily_mean, daily_min
 from .units import convert_temperature
 from .wind import wind_speed
@@ -27,6 +27,7 @@ __all__ = [
     "ensemble_spread",
     "hdd",
     "normalize_longitude",
+    "standardize",
     "weighted_mean",
     "wind_speed",
 ]
