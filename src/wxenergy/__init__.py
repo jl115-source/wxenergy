d45a15907f0131b3bdbcf0df21750889a68386be
jl115-source <jl115@alumni.princeton.edu>
2026-09-22 13:32:33 -0700

    Expose v0.2 public API

--- src/wxenergy/__init__.py
+++ src/wxenergy/__init__.py
@@ -5,17 +5,21 @@ from .energy import cdd, hdd
 from .spatial import weighted_mean
 from .standardize import normalize_longitude
 from .temporal import daily_max, daily_mean, daily_min
+from .units import convert_temperature
+from .wind import wind_speed
 
 __all__ = [
     "anomaly",
     "cdd",
     "climatology",
+    "convert_temperature",
     "daily_max",
     "daily_mean",
     "daily_min",
     "hdd",
     "normalize_longitude",
     "weighted_mean",
+    "wind_speed",
 ]
 
-__version__ = "0.1.0.dev0"
+__version__ = "0.2.0.dev0"
