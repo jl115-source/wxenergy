399a4a0d227eddc00da3b04700f82d49ae86c88f
jl115-source <jl115@alumni.princeton.edu>
2026-09-22 14:49:07 -0700

    Refine Sphinx documentation configuration

--- docs/conf.py
+++ docs/conf.py
@@ -10,6 +10,7 @@ version = release
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
     "sphinx.ext.napoleon",
 ]
 
@@ -18,11 +19,21 @@ autodoc_typehints = "description"
 napoleon_numpy_docstring = True
 napoleon_google_docstring = False
 
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "xarray": ("https://docs.xarray.dev/en/stable/", None),
+}
+
 html_theme = "pydata_sphinx_theme"
 html_title = "wxenergy"
 html_context = {"default_mode": "dark"}
+html_static_path = ["_static"]
+html_css_files = ["custom.css"]
 html_theme_options = {
     "navigation_with_keys": True,
+    "show_toc_level": 2,
+    "navbar_align": "left",
     "icon_links": [
         {
             "name": "GitHub",
